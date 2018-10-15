36a65342c5007d573581b78aa02d3919c156e861
indi3 <B.ziomek97@gmail.com>
2018-10-15 13:49:04 +0200

    Prerelease

--- src/MVC/Interface2.java
+++ src/MVC/Interface2.java
@@ -4,8 +4,10 @@ import RepoCurrency.Currency;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 import java.util.ArrayList;
 
@@ -13,14 +15,15 @@ public class Interface2 {
 
     private Calculator calc;
 
-    @FXML private TextField KwotaIn;
-    @FXML public ListView<String> Lista;
+    @FXML private TextField CountIn,CountOut,CodeIn,CodeOut;
+    @FXML private Button Exchange;
+    @FXML private ListView<String> Lista;
 
     @FXML
     private void initialize(){
         calc=new Calculator();
-
         ViewAll();
+        exchange();
     }
 
     private void ViewAll(){
@@ -32,4 +35,15 @@ public class Interface2 {
         }
         Lista.setItems(list);
     }
+
+    private void exchange(){
+        Exchange.setOnMouseClicked(new javafx.event.EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event) {
+                CountOut.setText(String.valueOf(calc.Calculate(Double.parseDouble(CountIn.getText()),CodeIn.getText(),CodeOut.getText())));
+            }
+        });
+    }
+
 }
+
